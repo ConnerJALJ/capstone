51c374e8dc26eb9b7af0b948d9e80871eaf2ccfb
ConnerJALJ <connerjalj@gmail.com>
2020-04-20 01:34:14 -0400

    initial commit

---
+++ capstone_container/entries.js
@@ -0,0 +1,12 @@
+var posts = JSON.stringify({
+  "January 30 2020": "Project proposal was submitted to advisor.",
+  "January 31 2020": "Advisor accepted project proposal.",
+  "February 05 2020": "Docker is now installed and set up successfully on the development system.",
+  "February 14 2020": "Completed the mock lay out the capstone page for tracking my progress.",
+  "February 15 2020": "Finished replacing mock elements with more permanent versions for the progress tracker",
+  "February 16 2020": "Added functionallity to extend the number of tracker posts shown on page.",
+  "February 27 2020": "Added functionallity for rapidly and easily creating posts in the progress tracker.",
+  "March 3 2020": "Initialized React.js application for the main site.",
+  "April 8 2020": "Set up basic information for the react project and deleted unneeded bloat."
+
+});
---
+++ main_container/client/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Menu from './Components/Menu/Menu';
+import Nav from './Components/Nav/Nav';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+
+App = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  )
+  
+}
+export default App;
